86f56ba536ae481d078d07fecd8191654d933d13
Wagner Araújo <wagneraraujo15@gmail.com>
2024-06-26 16:29:40 -0300

    feat: route login

--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -3,5 +3,6 @@ import UserController from "../controllers/UserController";
 
 const router = Router();
 router.post("/register", UserController.register);
+router.post("/login", UserController.login);
 
 export { router };
