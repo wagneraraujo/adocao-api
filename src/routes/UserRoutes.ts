d046f31445815c3443abe620be20e5af7db0b504
Wagner Araújo <wagneraraujo15@gmail.com>
2024-07-08 18:18:46 -0300

    feat: add routes checkuser and edit

--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -4,5 +4,7 @@ import UserController from "../controllers/UserController";
 const router = Router();
 router.post("/register", UserController.register);
 router.post("/login", UserController.login);
+router.get("/checkuser", UserController.checkUser);
+router.put("/edit/:id", UserController.editUser);
 
 export { router };
