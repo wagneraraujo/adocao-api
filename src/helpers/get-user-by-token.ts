84263cf4b346a48b2c295fe169a963d2a248b6d3
Wagner Araújo <wagneraraujo15@gmail.com>
2024-07-08 18:18:14 -0300

    feat: get user by token

---
+++ src/helpers/get-user-by-token.ts
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken";
+
+import User from "../models/User";
+import { StatusCodes } from "http-status-codes";
+
+const getUserByToken = async (token: string) => {
+  if (!token) {
+    return;
+  }
+
+  const verified: any = jwt.verify(token, "lauraalves");
+
+  const userId = verified.id;
+
+  const user = await User.findOne({ _id: userId });
+  return user;
+};
+
+export default getUserByToken;
