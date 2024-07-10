6f983c69caaff019ffe17443e7f18a23576006a3
Wagner Araújo <wagneraraujo15@gmail.com>
2024-07-10 19:26:58 -0300

    feat: verify token

--- src/helpers/get-user-by-token.ts
+++ src/helpers/get-user-by-token.ts
@@ -3,12 +3,12 @@ import jwt from "jsonwebtoken";
 import User from "../models/User";
 import { StatusCodes } from "http-status-codes";
 
-const getUserByToken = async (token: string) => {
+const getUserByToken = async (token: string | undefined) => {
   if (!token) {
-    return;
+    throw new Error("Token não fornecido"); // Ou retorne null, dependendo da sua lógica
   }
-
   const verified: any = jwt.verify(token, "lauraalves");
+  console.log(verified, "============ ============= ======== ========");
 
   const userId = verified.id;
 
---
+++ src/helpers/verify-token.ts
@@ -0,0 +1,36 @@
+import { Response, Request, NextFunction } from "express";
+import { StatusCodes } from "http-status-codes";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import getToken from "./getToken";
+
+//middleware for validate token
+const checkToken = (req: Request, res: Response, next: NextFunction) => {
+  console.log("verificar token");
+  const token = getToken(req, res, next);
+  const newToken: string | any = token;
+  console.log("token checkToken", token);
+  if (!req?.headers.authorization) {
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ message: "Acesso negado, sem headers" });
+  }
+
+  if (!token) {
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ message: "Acesso negado" });
+  }
+
+  try {
+    const verified = jwt.verify(newToken, "lauraalves") as JwtPayload;
+    (req as any).user = verified;
+    next();
+  } catch (error) {
+    console.log("erro checkToken", error);
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ message: "Acesso negado ou token inválido" });
+  }
+};
+
+export default checkToken;
