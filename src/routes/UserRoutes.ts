2cab7d8363f758137689bfa79200ba11fe8d5f82
Wagner Araújo <wagneraraujo15@gmail.com>
2024-07-10 10:53:43 -0300

    update routes

--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import UserController from "../controllers/UserController";
+import getToken from "../helpers/getToken";
 
 const router = Router();
 router.post("/register", UserController.register);
 router.post("/login", UserController.login);
 router.get("/checkuser", UserController.checkUser);
-router.put("/edit/:id", UserController.editUser);
+router.get("/:id", UserController.getUserById);
+router.patch("/edit/:id", UserController.editUser);
 
 export { router };
