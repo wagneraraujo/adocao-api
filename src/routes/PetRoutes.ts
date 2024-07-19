2de56df35c6a241e61d3fa26087e6f05816483d7
Wagner Araújo <wagneraraujo15@gmail.com>
2024-07-18 23:00:40 -0300

    feat: pet route update

--- src/routes/PetRoutes.ts
+++ src/routes/PetRoutes.ts
@@ -1,7 +1,13 @@
 import { Router } from "express";
 import PetController from "../controllers/PetController";
 import checkToken from "../helpers/verify-token";
+import imageUploud from "../helpers/imageUploud";
 const petRouter = Router();
-petRouter.post("/create", checkToken, PetController.create);
+petRouter.post(
+  "/create",
+  checkToken,
+  imageUploud.array("images"),
+  PetController.create,
+);
 
 export default petRouter;
